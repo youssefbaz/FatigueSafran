466809c53128b889a18d32fa6da717980a52e664
youssefbaz <bazzaoui.youssef@gmail.com>
2022-06-25 20:52:02 +0200

    rename page according to content

---
+++ pages/failure_proba.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from scripts.Failure_proba2 import calculate_failure
+from PIL import Image
+import os
+
+
+BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "images"))
+
+
+# st.markdown("# Page 4 ❄️")
+# st.sidebar.markdown("# Page 4 ❄️")
+
+def failure_probability():
+    Failure_probability = st.button("Calculation of the failure probability ")
+    if Failure_probability:
+        gamma = st.session_state.config['gamma']
+        status=st.text("calculating using gamma "+str(gamma))
+        fig, fig1 = calculate_failure(gamma)
+        st.write(fig)
+        st.write(fig1)
+        status.text("done calculation")
+
+# failure_probability()
+
